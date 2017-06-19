180690f4c795a1c221385b6cb4d3f7508b0a6377
Shimon Shvartsbroit <shimons@wix.com>
2017-06-19 13:18:36 +0300

    chore: add place for video and movie search text field

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {translate} from 'react-i18next';
 import s from './App.scss';
+import {Container, Row, Col, Card} from 'wix-style-react/dist/src/Grid';
+// import {Container, Row, Col, Card} from 'wix-style-react/dist/src/Grid';
+import {TextField, Input} from 'wix-style-react';
+// import {TextField} from 'wix-style-react/dist/src/TextField';
+// import {Input} from 'wix-style-react/dist/src/Input';
+// import {Label} from 'wix-style-react/dist/src/Label';
+function renderStandardInput() {
+  return (
+    <TextField>
 
-function App({t}) {
+      <Input
+        id="textField"
+        placeholder="Default text goes"
+        />
+    </TextField>
+  );
+}
+function App() {
+
   return (
     <div className={s.root}>
       <div className={s.header}>
-        <h2>{t('app.title')}</h2>
+        <h2>Awesome Video Player</h2>
       </div>
-      <p className={s.intro}>
-        {t('app.intro')}
-      </p>
+
+
+      <Container>
+        <Row>
+          <Col span={12}>
+            <Card>
+              <Card.Header title="Search Movies"/>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={4}>
+            {renderStandardInput()}
+          </Col>
+
+          <Col span={8}>
+            <div className={s.videoplayer}/>
+          </Col>
+        </Row>
+
+      </Container>
+
     </div>
   );
 }
