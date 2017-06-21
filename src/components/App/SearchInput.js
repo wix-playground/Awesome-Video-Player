1b5f102b5f452ecbb513dd9631800029b7f0f071
Shimon Shvartsbroit <shimons@wix.com>
2017-06-21 16:49:10 +0300

    add onChange event to SearchInput

--- src/components/App/SearchInput.js
+++ src/components/App/SearchInput.js
@@ -4,11 +4,12 @@ import s from './App.scss';
 
 class SearchInput extends React.Component {
   render() {
+    const {placeholder, onChange} = this.props;
     return (
       <div>
         <div className={s.searchInputTextField}>
           <TextField>
-            <Input id="movieNameTextField" placeholder={this.props.placeholder}/>
+            <Input id="movieNameTextField" placeholder={placeholder} onChange={onChange}/>
           </TextField>
         </div>
         <Button height="medium" theme="fullblue" >Search!</Button>
